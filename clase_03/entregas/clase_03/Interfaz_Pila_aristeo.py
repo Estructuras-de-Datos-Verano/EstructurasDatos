1acc83a3bef8f0ef34508f92568330adfe5d6afa
AristeoMucito <0281528@up.edu.mx>
2026-06-18 11:11:40 -0600

    Create Interfaz_Pila_aristeo.py

---
+++ clase_03/entregas/clase_03/Interfaz_Pila_aristeo.py
@@ -0,0 +1,17 @@
+class Pila:
+
+    def push(self, objeto):
+        "añadir al tope (final de la lista)"
+        pass
+    def pop(self):
+        "quitar y devolver el tope; devolver None si está vacía"
+        pass
+    def peek(self):
+        "devolver el tope sin eliminar; None si está vacía"
+        pass
+    def esta_vacia(self):
+        "True si no hay elementos"
+        pass
+    def tamano(self):
+        "número de elementos actuales"
+        pass
