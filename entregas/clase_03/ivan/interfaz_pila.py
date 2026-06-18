94b3d8d4abd55ce78a9fea40e294093e6852c334
ELinceG <elince@up.edu.mx>
2026-06-18 11:26:16 -0600

    Correciones

--- entregas/clase_03/ivan/interfaz_pila.py
+++ entregas/clase_03/ivan/interfaz_pila.py
@@ -1,20 +1,17 @@
-from abc import ABC, abstractmethod
-from typing import Any
-class Pila(ABC):
+
+class Pila:
 
     def __init__(self) -> None:
         """Inicializa la pila vacía."""
         pila = []
         self.pila = pila
 
-    @abstractmethod
     def push(self, elemento: Any) -> None:
         """Agrega un elemento a la pila."""
 
         self.pila.append(elemento)
 
 
-    @abstractmethod
     def pop(self, elemento: Any) -> Any:
         """Elimina y regresa el elemento en la cima de la pila.
 
@@ -22,7 +19,6 @@ class Pila(ABC):
         """
         self.pila = self.pila[:-1]
 
-    @abstractmethod
     def peek(self) -> Any:
         """Regresa el elemento en la cima de la pila sin modificarla.
 
@@ -30,7 +26,6 @@ class Pila(ABC):
         """
         return self.pila[-1]
 
-    @abstractmethod
     def empty(self) -> Bool:
         """Regresa un valor verdadero o falso según el estado de la pila
 
