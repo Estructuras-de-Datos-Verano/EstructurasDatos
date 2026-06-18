cabb892865a10d46a0fa1362d519c75c9ee270f1
Arturo Prudencio Bonilla <0255295@up.edu.mx>
2026-06-18 11:12:18 -0600

    Revision de Arturo -> Santiago Vazquez
    hice la implementación de la interfaz que propuso Santi

--- entregas/clase_03/Santiago_Vazquez_Serna/Interfaz_Pila.py
+++ entregas/clase_03/Santiago_Vazquez_Serna/Interfaz_Pila.py
@@ -1,17 +1,32 @@
 class Pila:
+    def __init__(self):
+        self.pila = []
 
     def push(self, objeto):
         "añadir al tope (final de la lista)"
-        pass
+        self.pila.append(objeto)
+
     def pop(self):
         "quitar y devolver el tope; devolver None si está vacía"
-        pass
+        if len(self.pila) == 0:
+            return None
+        else:
+            tope = self.pila[-1]
+            self.pila.remove(tope)
+        return tope
+
     def peek(self):
         "devolver el tope sin eliminar; None si está vacía"
-        pass
+        if len(self.pila) == 0:
+            return None
+        else:
+            tope = self.pila[-1]
+        return tope
+    
     def esta_vacia(self):
         "True si no hay elementos"
-        pass
+        return len(self.pila) == 0
+    
     def tamano(self):
         "número de elementos actuales"
-        pass
+        return f"La pila tiene {len(self.pila)} elementos"
