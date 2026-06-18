efe8475198b0b6e33d1412eafc4304b98812ef00
San-tiagoVS <0285489@up.edu.mx>
2026-06-18 11:13:06 -0600

    Revisión de Santiago Vázquez-> Tiago Saldívar
    Hice la implementación y la revisión de la interfaz de Tiago. revise el código y añadí lo restante

--- entregas/clase_03/tiago/interfaz-pila.py
+++ entregas/clase_03/tiago/interfaz-pila.py
@@ -2,29 +2,43 @@ class pilaTDA:
     """
     Propuesta de interfaz de pila usando POO.
     """
-        
+    def __init__(self):
+        self.pila = []
+            
     def push(self,objeto):
         """
         Agrega un elemento hasta arriba de la pila.
         """
-
-        pass
-
-
+        self.pila.append(objeto)
+        
     def pop(self):
         """
         Remueve el tope.
         """
-        pass
+        if len(self.pila) == 0:
+            return None
+
+        else:
+            tope = self.pila[-1]
+            self.pila.remove(tope)
+
+        return tope
 
     def peek(self):
         """
         Nos permite ver el tope. No lo remueve ni actualiza la pila.
         """
-        pass
+        if len(self.pila) == 0:
+            return None
+        
+        else:
+         tope = self.pila[-1]
+
+         return tope
+
     def empty(self):
         """
         Revisa si la lista es vacía. Regresa un booleano.
         """
-
-        pass        
+        return len(self.pila) == 0
+                
